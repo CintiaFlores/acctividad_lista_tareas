07a7e291fd27770c9687bee2969c1212b49b99d8
Cintia Flores <cintia.flores.0502@gmail.com>
2023-10-06 14:02:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// Función para agregar una nueva tarea
+function agregarTarea() {
+    const nuevaTareaInput = document.getElementById("nuevaTarea");
+    const listaTareas = document.getElementById("listaTareas");
+    const nuevaTareaTexto = nuevaTareaInput.value.trim();
+    if (nuevaTareaTexto !== "") {
+        const nuevaTarea = document.createElement("li");
+        nuevaTarea.textContent = nuevaTareaTexto;
+
+        const botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+        botonEliminar.className = "delete";
+        botonEliminar.onclick = function () {
+            listaTareas.removeChild(nuevaTarea);
+        };
+
+        const botonMarcarCompletada = document.createElement("button");
+        botonMarcarCompletada.textContent = "Completada";
+        botonMarcarCompletada.className = "success";
+        botonMarcarCompletada.onclick = function () {
+            marcarCompletada(nuevaTarea);
+        };
+
+        nuevaTarea.appendChild(botonMarcarCompletada);
+        nuevaTarea.appendChild(botonEliminar);
+        listaTareas.appendChild(nuevaTarea);
+        nuevaTareaInput.value = "";
+    }
+}
+// Función para marcar una tarea como completada
+function marcarCompletada(tarea) {
+    tarea.classList.toggle("completed");
+}
+// Agregar evento de clic a las tareas para marcarlas como completadas
+document.getElementById("listaTareas").addEventListener("click",
+    function (event) {
+        if (event.target.tagName === "LI") {
+            marcarCompletada(event.target);
+        }
+    });
+// Agregar evento de clic al botón "Agregar"
+document.getElementById("agregar").addEventListener("click", agregarTarea);
+
+// --------------------- TAREAS --------------------- //
+// Función para mostrar tareas completadas
+function mostrarCompletadas() {
+    const tareas = document.querySelectorAll("li");
+    tareas.forEach(tarea => {
+        if (tarea.classList.contains("completed")) {
+            tarea.style.display = "flex";
+        } else {
+            tarea.style.display = "none";
+        }
+    })
+}
